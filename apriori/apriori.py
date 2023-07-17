91b689494f0532b845a58353f76e6d85cc35853b
pfenningpat <pat.pfenning@blastpoint.com>
2023-07-17 11:29:06 -0400

    return 1-tuple

--- apriori/apriori.py
+++ apriori/apriori.py
@@ -21,33 +21,33 @@ def __get_df(transactions, k):
 
 def __get_items(freq, k):
     """Get set of items which are above support at level k"""
-    # first iteration has no tuples
-    if k == 1:
-        return freq.keys()
     # concatenate all tuples and return a set to dedupe
-    return set(list(reduce(operator.concat, freq.keys())))
+    return set(list(reduce(operator.concat, freq)))
 
 
 def __get_freq(transactions, supp_cnt, k):
     """Get frequency itemset at k^th support"""
     df = __get_df(transactions, k)
     # check tuples which are above support, get columns and return sum to show support
-    freq = df[df.columns[(df.sum() >= supp_cnt)]].sum().to_dict()
+    freq = list(df.columns[(df.sum() >= supp_cnt)])
+    if k == 1:
+        freq = {(v,) for v in freq}
     if freq:
         return freq, __get_items(freq, k)
-    return freq, None
+    else:
+        return freq, None
 
 
 def apriori(transactions, min_support):
     supp_cnt = np.floor(min_support*transactions.shape[0])
-    frequency_itemset = dict()
+    frequency_itemset = []
     k = 1
     while True:
         freq, items = __get_freq(transactions, supp_cnt, k)
         # if there are items left to choose from then continue
         if items:
             # update frequency dictionary each step
-            frequency_itemset.update(freq)
+            frequency_itemset += freq
             # increase level
             k += 1
             # limit transactions to rows which purchased at min k items, only use columns in item list
